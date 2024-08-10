170fb1c23defb32135ca01344a5d3685ae639f16
Rylan <1217013295@qq.com>
2024-08-10 22:17:39 +0800

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const BLOG = require('./blog.config');
 const { fontFamilies } = require('./lib/font');
 
 module.exports = {
+  important: true,
   content: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js', './themes/**/*.js'],
   darkMode: BLOG.APPEARANCE === 'class' ? 'media' : 'class', // or 'media' or 'class'
   theme: {
